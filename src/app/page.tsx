b427dc2b00e679eb46f61fe8fa58b15c58f5fa28
edwardyh80 <edwardyh80@gmail.com>
2025-01-24 10:41:51 +0900

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,70 @@
+"use client";
+
+import { RefObject, useEffect, useRef, useState } from "react";
+
+import { twMerge } from "tailwind-merge";
+
 import Details from "@/components/Details";
+import { sections } from "@/components/Details";
 import Profile from "@/components/Profile";
 
-const Home = () => (
-  <div className="m-auto flex max-w-screen-xl flex-col gap-16 px-4 lg:flex-row lg:px-8">
-    <Profile />
-    <Details />
-  </div>
+const Nav = ({
+  vis,
+  refList,
+}: {
+  vis: boolean[];
+  refList: RefObject<HTMLElement[]>;
+}) => (
+  <nav className="hidden lg:sticky lg:top-0 lg:flex lg:h-screen lg:flex-col lg:justify-center lg:overflow-y-auto lg:py-24">
+    <ul className="flex flex-col justify-center gap-1 text-center">
+      {sections.map((s, i) => (
+        <li
+          key={s.id}
+          className={twMerge(
+            "cursor-pointer p-1 text-xs font-semibold uppercase",
+            vis[i] && "text-secondary-400",
+          )}
+          onClick={() => {
+            if (refList.current)
+              refList.current[i].scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          {s.label[0]}
+        </li>
+      ))}
+    </ul>
+  </nav>
 );
+
+const Home = () => {
+  const [vis, setVis] = useState(new Array(sections.length).fill(false));
+  const refList = useRef<HTMLElement[]>(new Array(sections.length).fill(null));
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const delta = new Array(sections.length).fill(null);
+      entries.forEach((e) => {
+        delta[sections.findIndex((s) => s.id === e.target.id)] =
+          e.isIntersecting;
+      });
+      setVis((v) => delta.map((d, i) => (d === null ? v[i] : d)));
+    });
+
+    refList.current.forEach((r) => {
+      observer.observe(r);
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, [refList]);
+
+  return (
+    <div className="m-auto flex max-w-screen-xl flex-col gap-16 px-4 lg:flex-row lg:gap-8 lg:px-8">
+      <Profile />
+      <Nav vis={vis} refList={refList} />
+      <Details refList={refList} />
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,32 +1,46 @@
+import { RefObject } from "react";
+
 import About from "./About";
 import Experience from "./Experience";
 import Footer from "./Footer";
 import Freelance from "./Freelance";
 import Interests from "./Interests";
 
-const sections = [
+export const sections = [
   {
+    id: "about",
     label: "About",
     content: About,
   },
   {
+    id: "interests",
     label: "Interests",
     content: Interests,
   },
   {
+    id: "experience",
     label: "Experience",
     content: Experience,
   },
   {
+    id: "freelance-projects",
     label: "Freelance Projects",
     content: Freelance,
   },
 ];
 
-const Details = () => (
+const Details = ({ refList }: { refList: RefObject<HTMLElement[]> }) => (
   <main className="flex flex-col gap-16 pb-16 lg:py-24">
-    {sections.map((s) => (
-      <section key={s.label} className="prose prose-invert">
+    {sections.map((s, i) => (
+      <section
+        key={s.id}
+        id={s.id}
+        className="prose prose-invert"
+        style={{ scrollMargin: 64 }}
+        ref={(n) => {
+          if (refList.current && n) refList.current[i] = n;
+        }}
+      >
         <h3 className="text-xs font-semibold uppercase tracking-widest text-secondary-400">
           {s.label}
         </h3>
